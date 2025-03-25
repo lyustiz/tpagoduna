9f21651038c83af44bfee43a142773fd8b313024
Luis Yustiz <lyustiz@gmail.com>
2025-03-25 08:44:02 -0400

    cambio color de ticket

--- resources/js/Pages/Compra/Components/TicketItem.jsx
+++ resources/js/Pages/Compra/Components/TicketItem.jsx
@@ -6,10 +6,10 @@ const TicketItem = memo(({ ticketsSel, ticket, onAddTicket, onRemoveTicket }) =>
   const isSelected = ticketsSel.some((t) => t.nu_numero === ticket.nu_numero);
 
   const estados = {
-    3: { color: "#2e7d32", label: "Disponible", active: true , "code" : '#2e7d32'},
-    4: { color: "warning", label: "Reservado", active: false, "code" : '#ed6c02' },
-    5: { color: "error", label: "Vendido", active: false, "code" : '#d32f2f' },
-    0: { color: "info", label: "No disponible", active: false, "code" : '#d32f2f' },
+    3: { color: "#2e7d32", label: "Disponible", active: true , "code" : '#2e7d32', textColor: 'inherit' },
+    4: { color: "warning", label: "Reservado", active: false, "code" : '#ed6c02', textColor: 'inherit' },
+    5: { color: "error", label: "Vendido", active: false, "code" : '#d32f2f', textColor: '#d32f2f' },
+    0: { color: "info", label: "No disponible", active: false, "code" : '#d32f2f', textColor: 'inherit' },
   };
 
   const estado = estados[ticket.id_estado] ?? estados[0]; ;
@@ -49,7 +49,7 @@ const TicketItem = memo(({ ticketsSel, ticket, onAddTicket, onRemoveTicket }) =>
           clickable={estado.active}
           onClick={estado.active ? handleClick : null}
           message={isSelected ? "active" : "inactive"}
-          sx={{ fontSize: '1.20rem', fontWeight: 'bold' }}
+          sx={{ fontSize: '1.20rem', fontWeight: 'bold', color: estado.textColor }}
         ></Chip>
       </Tooltip>
     </>
